27e0c4652c6088c2b821bdd87bbd2ad68851920e
dpthegrey <dpthegrey@gmail.com>
2021-06-02 14:49:28 +0530

    Fixing a problem with disappearing users

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -110,6 +110,13 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+
+		case "left":
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
 		}
 
 		// response.Action = "Got here"
